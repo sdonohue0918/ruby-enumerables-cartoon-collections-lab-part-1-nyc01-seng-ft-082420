825e56b995e2fca16c43c43f9a5f7f6becb16271
observant-mapping-4330 <sdonohue0918@gmail.com>
2020-08-05 18:47:17 +0000

    Done.

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,11 +1,28 @@
+
+
+characters_array = ["Hoggle", "Ludo", "Sir Didymus"]
+
+
+
 def greet_characters(array)
   # Use `each` to enumerate over the provided array
   #
   # Print a custom greeting for each element
+
+  array.each {|el| print "Hello #{el}! \n"}
+
+
+
 end
 
 def list_dwarves(array)
   # Use `each_with_index` to enumerate over the provided array
   #
   # Print a numbered list of each element
+
+  print array.each_with_index {|element, index| print "#{index + 1}. #{element} "}
+
+
+
+
 end
