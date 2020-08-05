34a2c26401681e6315cff7f66c37595590250a63
observant-mapping-4330 <sdonohue0918@gmail.com>
2020-08-05 18:47:05 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -19,4 +19,11 @@ def list_dwarves(array)
   # Use `each_with_index` to enumerate over the provided array
   #
   # Print a numbered list of each element
+  
+  print array.each_with_index {|element, index| print "#{index + 1}. #{element} "}
+
+
+  
+  
 end
+    
